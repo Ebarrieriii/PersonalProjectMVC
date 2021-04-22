f4cfdd64175ceeb5c0f70ed5534059793a28d74f
edwardiiibarrier@gmail.com <edwardiiibarrier@gmail.com>
2021-04-21 19:51:18 -0500

    Convcerted to MVC

--- PersonalProjectMVC/PersonalProjectMVC.UI/App_Start/BundleConfig.cs
+++ PersonalProjectMVC/PersonalProjectMVC.UI/App_Start/BundleConfig.cs
@@ -22,9 +22,27 @@ namespace PersonalProjectMVC.UI
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                    "~/Content/vendor/bootstrap/js/bootstrap.bundle.min.js",
+                    "~/Content/vendor/php-email-form/validate.js",
+                    "~/Content/vendor/counterup/counterup.min.js",
+                    "~/Content/vendor/owl.carousel/owl.carousel.min.js",
+                    "~/Content/vendor/isotope-layout/isotope.pkgd.min.js",
+                    "~/Content/vendor/venobox/venobox.min.js",
+                    "~/Scripts/js/main.js"
+                ));
+
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/vendor/bootstrap/css/bootstrap.min.css",
+                      "~/Content/vendor/icofont/icofont.min.css",
+                       "~/Content/vendor/remixicon/remixicon.css",
+                       "~/Content/vendor/owl.carousel/assets/owl.carousel.min.css",
+                       "~/Content/vendor/boxicons/css/boxicons.min.css",
+                       "~/Content/vendor/venobox/venobox.css",
+                       "~/Content/css/style.css"
+                      ));
         }
     }
 }
